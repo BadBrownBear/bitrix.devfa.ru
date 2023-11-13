88e150646bcbf52da6cf2181f6ba941b56c5fd26
Daria Smirnova <pinkalterelephant@gmail.com>
2023-11-13 11:03:51 +0300

    Шаблон bootstrap

--- local/templates/main/footer.php
+++ local/templates/main/footer.php
@@ -1,6 +1,7 @@
 <?php
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 ?>
-
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 </body>
 </html>
--- local/templates/main/header.php
+++ local/templates/main/header.php
@@ -6,7 +6,9 @@ IncludeTemplateLangFile(__FILE__);
 <html lang="en">
 <head>
     <?$APPLICATION->ShowHead();?>
+    <link htrf="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
     <title><?php $APPLICATION->ShowTitle();?></title>
 </head>
 <body>
 <?$APPLICATION->ShowPanel()?>
+<div class="container">
